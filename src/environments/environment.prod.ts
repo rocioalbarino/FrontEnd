85bdfdc4df324f7f32b34bc690d0ba811a56abe6
rocioalbarino <100244315+rocioalbarino@users.noreply.github.com>
2022-10-28 09:33:02 -0300

    Cambios hys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  // URL: 'https://heroku-backendmgb.herokuapp.com/'
+
+  URL: 'https://backendrocioalba.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
 
   production: false,
-  URL: 'http://localhost:8080/'
+
+  URL: 'http://localhost:8080/',
 };
 
 /*
